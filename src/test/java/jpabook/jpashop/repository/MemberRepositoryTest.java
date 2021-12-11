8400671d0d0679a5cf18fcf5acb8dad2d7879adf
ksungsu <ksungsu@naver.com>
2021-12-11 21:38:19 +0900

    TIL 50th day //@Rollback //p6spy

--- src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
@@ -6,9 +6,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
@@ -19,6 +21,7 @@ public class MemberRepositoryTest {
 
     @Test
     @Transactional
+    @Rollback(false)
     public void  testMember() {
         //given
         Member member = new Member();
@@ -29,8 +32,10 @@ public class MemberRepositoryTest {
         Member findMember = memberRepository.find(savedId);
 
         //then
-        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);
 
+        System.out.println("findMember == member = " + (findMember == member));
     }
 }
