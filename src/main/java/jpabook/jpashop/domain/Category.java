5cf3e6c86944bff9714ce50b5c26a6dea9441b1b
ksungsu <ksungsu@naver.com>
2021-12-14 00:20:00 +0900

    TIL 52th day //엔티티 설계시 주의점 //회원 리포지토리 개발

--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -8,6 +8,8 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.*;
+
 @Entity
 @Getter @Setter
 public class Category {
@@ -24,10 +26,17 @@ public class Category {
             inverseJoinColumns = @JoinColumn(name = "item_id"))
     private List<Item> items = new ArrayList<>();
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "parent_id")
     private Category parent;
 
     @OneToMany(mappedBy = "parent")
     private List<Category> child = new ArrayList<>();
+
+    //Category가 부모에도 들어가고 자식에도 들어가야한다.
+    //==연관관계 편의 메서드==//
+    public void addChildCategory(Category child){
+        this.child.add(child);
+        child.setParent(this);
+    }
 }
--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -8,6 +8,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.*;
+
 /**
  * Create order Entity
  */
@@ -21,14 +23,14 @@ public class Order {
     @Column(name="order_id")
     private Long id;
 
-    @ManyToOne  //멤버와 다(order)대일(member) 관계
+    @ManyToOne(fetch = LAZY)  //멤버와 다(order)대일(member) 관계
     @JoinColumn(name = "member_id") //외래키 설정, 이름 설정
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -36,4 +38,27 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderState orderState; //주문상태 [ORDER, CANCEL]
+
+//    public static void main(String[] args) {
+//        Member member = new Member();
+//        Order order = new Order();
+//
+//        member.getOrders().add(order);
+//        order.setMember(member);
+//    }
+    //==연관관계 편의 메서드==// 위 주석된 코드를 아래 메서드와 같이 설정 가능.
+        public void setMember(Member member){
+            this.member = member;
+            member.getOrders().add(this);   //this is ordered person
+        }
+
+        public void addOrderItem(OrderItem orderItem){
+            orderItems.add(orderItem);
+            orderItem.setOrder(this);
+        }
+
+        public void setDelivery(Delivery delivery){
+            this.delivery = delivery;
+            delivery.setOrder(this);
+        }
 }
