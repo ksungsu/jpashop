a9e5944d03d81aec4ddbc9aed9593ac403ecb4b8
ksungsu <zxzxzlxlzlxl@naver.com>
2021-12-17 14:14:05 +0900

    TIL 55th day // 주문(Order), 주문 상품(OrderItem) 엔티티 개발 // 주문 리포지토리 개발 // 주문 서비스 개발

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -26,4 +26,28 @@ public class OrderItem {
 
     private int orderPrice; //주문 당시 가격(제품 가격은 바뀔 수 있으므로)
     private int count;  //주문 수량
+
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count); //count 만큼 재고 감소
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    public void cancel(){
+        getItem().addStock(count); //재고 상태 원복
+    }
+    //==조회 로직==//
+
+    /**
+     * 주문상품 전체 가격 조회
+     */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
